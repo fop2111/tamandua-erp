af1b93b96ff6d6622cbfc25e1cd06aaa0c3ece6b
MarlonTamanduaERP <MarlonTamanduaERP@gmail.com>
2011-02-17 01:38:25 +0000

    Criada a pakage persistencia.impl

---
+++ DespesasPessoais/src/persistencia/impl/GatewayEspeciePagamento.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package persistencia.impl;
+
+import excecao.PessoaisException;
+import java.awt.Desktop;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import modelo.dominio.EspeciePagamento;
+import persistencia.IGatewayEspeciePagamento;
+
+/**
+ *
+ * @author Marlon
+ */
+public class GatewayEspeciePagamento implements IGatewayEspeciePagamento {
+
+    public void gravarEspeciePagamento(EspeciePagamento especie_pagamento) throws PessoaisException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private static final String SQL_INCLUIR_ESPECIE_PAGAMENTO = "INSERT INTO tam_custos.especie_pagamento " +
+                                                                        "(id_especie_pagamento, descricao, operacao , tipo_conta "+
+                                                                 "VALUES (?, ?, ?, ?)";
+
+
+   private void incluirSocio(EspeciePagamento especie_pagamento) throws PessoaisException {
+     if (especie_pagamento == null) {
+       String mensagem = "Não foi informado o socio a cadastrar.";
+       throw new PessoaisException(mensagem);
+     }
+     Connection con         = null;
+     PreparedStatement stmt = null;
+     try {
+            con = GerenciadorDeConexao.getConexao();
+            stmt = con.prepareStatement(SQL_INCLUIR_ESPECIE_PAGAMENTO);
+            //GeradorDeChave geradorDeChave = new GeradorDeChave("socio");
+            long codigosocio = 1 ;
+            stmt.setLong(1, codigosocio);
+            stmt.setString(2, especie_pagamento.getDescricao());
+            stmt.setString(3, especie_pagamento.getOperacao());
+            stmt.setString(4, especie_pagamento.getTipo_conta());
+
+            stmt.executeUpdate();
+            } catch (SQLException exc) {
+                    StringBuffer mensagem = new StringBuffer("Não foi possível incluir o sócio.");
+                    mensagem.append("\nMotivo: " + exc.getMessage());
+                    throw new PessoaisException(mensagem.toString());
+              } finally {
+                    GerenciadorDeConexao.closeConexao(con, stmt);
+                }
+}
+
+}
