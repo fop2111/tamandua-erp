b055e51ed1127b1ebd6d3425336aa34177d23936
MarlonTamanduaERP@gmail.com <MarlonTamanduaERP@gmail.com@258e5cc1-fbd4-badc-4d7e-720efd2f1d00>
2011-02-17 01:04:38 +0000

    Criada as Conexoes e outras pakages

---
+++ trunk/DespesasPessoais/src/modelo/dominio/EspeciePagamento.java
@@ -0,0 +1,80 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modelo.dominio;
+
+/**
+ *
+ * @author Marlon
+ */
+public class EspeciePagamento {
+
+    private int id_especie_pagamento = 0 ;
+    private String descricao         = "";
+    private String operacao          = "";
+    private String tipo_conta        = "";
+
+    public EspeciePagamento() {
+    }
+
+    /**
+     * @return the id_especie_pagamento
+     */
+    public int getId_especie_pagamento() {
+        return id_especie_pagamento;
+    }
+
+    /**
+     * @param id_especie_pagamento the id_especie_pagamento to set
+     */
+    public void setId_especie_pagamento(int id_especie_pagamento) {
+        this.id_especie_pagamento = id_especie_pagamento;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the operacao
+     */
+    public String getOperacao() {
+        return operacao;
+    }
+
+    /**
+     * @param operacao the operacao to set
+     */
+    public void setOperacao(String operacao) {
+        this.operacao = operacao;
+    }
+
+    /**
+     * @return the tipo_conta
+     */
+    public String getTipo_conta() {
+        return tipo_conta;
+    }
+
+    /**
+     * @param tipo_conta the tipo_conta to set
+     */
+    public void setTipo_conta(String tipo_conta) {
+        this.tipo_conta = tipo_conta;
+    }
+
+    
+
+}
---
+++ trunk/DespesasPessoais/src/modelo/dominio/constante/Constante.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modelo.dominio.constante;
+
+/**
+ *
+ * @author Marlon
+ */
+public interface Constante {
+ public static final byte NOVO = -1;
+}
