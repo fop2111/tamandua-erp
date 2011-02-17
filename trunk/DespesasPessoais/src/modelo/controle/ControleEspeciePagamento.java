884bdcf18ab0585ca7ed1784500f8d81172c81d8
MarlonTamanduaERP@gmail.com <MarlonTamanduaERP@gmail.com@258e5cc1-fbd4-badc-4d7e-720efd2f1d00>
2011-02-17 02:46:37 +0000

    Criada a pakage persistencia.impl

---
+++ trunk/DespesasPessoais/src/modelo/controle/ControleEspeciePagamento.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modelo.controle;
+
+import excecao.PessoaisException;
+import modelo.dominio.EspeciePagamento;
+import persistencia.IFabricaPersistencia;
+import persistencia.IGatewayEspeciePagamento;
+import persistencia.impl.FabricaPersistencia;
+
+/**
+ *
+ * @author Marlon
+ */
+public class ControleEspeciePagamento {
+
+    public void gravarSocio(EspeciePagamento especie_pagamento) throws PessoaisException {
+        IFabricaPersistencia fabricaPersistencia = new FabricaPersistencia();
+        IGatewayEspeciePagamento gatewayEspeciePagamento = fabricaPersistencia.getGatewayEspeciePagamento();
+        gatewayEspeciePagamento.gravarEspeciePagamento(especie_pagamento);
+    }
+}
---
+++ trunk/DespesasPessoais/src/persistencia/impl/GeradorDeChave.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package persistencia.impl;
+
+import excecao.PessoaisException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Marlon
+ */
+public class GeradorDeChave {
+
+    private Connection con;
+    private String tabela;
+
+    public GeradorDeChave(String tabela) throws PessoaisException {
+       this.con = GerenciadorDeConexao.getConexao();
+       this.tabela = tabela;
+       try {
+            con.setAutoCommit(false);
+           } catch (SQLException exc) {
+                StringBuffer mensagem = new StringBuffer ("Não foi possível desligar confirmação automática");
+                mensagem.append("\nMotivo: " + exc.getMessage());
+             throw new PessoaisException(mensagem.toString());
+           }
+    }
+
+    public synchronized long getProximoCodigo()throws PessoaisException {
+
+        PreparedStatement stmt = null;
+        ResultSet rs           = null;
+        long proximoCodigoNovo = 0;
+        String sql = "select  nextval('?') proximoCodigo ";
+        try {
+              stmt = con.prepareStatement(sql);
+              stmt.setString(1, tabela);
+              rs = stmt.executeQuery();
+              rs.next();
+              proximoCodigoNovo = rs.getLong("proximoCodigo");
+            } catch (SQLException exc) {
+                  StringBuffer mensagem = new StringBuffer("Não foi possível gerar o próximo código");
+                  mensagem.append("\nMotivo: " + exc.getMessage());
+                  throw new PessoaisException(mensagem.toString());
+              }finally {
+                  GerenciadorDeConexao.closeConexao(con, stmt, rs);
+               }
+         return proximoCodigoNovo;
+     }
+}
